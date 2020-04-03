e975b8c482dad68fb3156da63472f72126483b73
minal <minalkewat@gmail.com>
2020-04-04 01:08:37 +0530

    Initial Commit

---
+++ PassingDataWithClosure/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PassingDataWithClosure
+//
+//  Created by apple on 04/04/20.
+//  Copyright © 2020 apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
